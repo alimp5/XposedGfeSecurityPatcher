94c24bb37148e45d9e26b049478c412e00a8bca7
Darren Coles <darren_m_coles@yahoo.co.uk>
2014-06-03 17:49:01 +0100

    updated to handle latest good client

--- src/com/phantasm/xposed/gfesecuritypatcher/GfeHook.java
+++ src/com/phantasm/xposed/gfesecuritypatcher/GfeHook.java
@@ -6,7 +6,6 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-
 public class GfeHook implements IXposedHookLoadPackage {
 	private static final String PACKAGE_EMAIL = "com.good.android.gfe";
 	
@@ -25,22 +24,108 @@ public class GfeHook implements IXposedHookLoadPackage {
     		XposedBridge.log("class not found : com.good.android.compliance.ComplianceManagerPolicy");
     	}
     	
-    	final Class<?> classSecurityPolicy = XposedHelpers.findClass("g.aeb", lpparam.classLoader);
+    	//older version of gfe
+    	Class<?> classSecurityPolicy = XposedHelpers.findClass("g.aeb", lpparam.classLoader);
     	if (classSecurityPolicy!=null) {
     		XposedBridge.log("class found : g.aeb");
-    	} else {
-    		XposedBridge.log("class not found : g.aeb");
+			de.robv.android.xposed.XC_MethodHook.Unhook u;			
+			try
+			{
+				u = XposedHelpers.findAndHookMethod(classSecurityPolicy, "a", classPolicy, 
+						XC_MethodReplacement.returnConstant(Integer.valueOf("0"))
+        			);
+			}
+			catch (java.lang.Throwable e)
+			{
+				u = null; 
+				XposedBridge.log("hook1 g.aeb fail");
+			}				
+       	
+        	if (u!=null) {
+        		XposedBridge.log("hook1 successful");
+        	} else {
+        		XposedBridge.log("hook1 failed");   	
+        	}
+
     	
+    	} else {
+    		XposedBridge.log("class not found : g.aeb");    	
     	}
     	
-    	de.robv.android.xposed.XC_MethodHook.Unhook u = XposedHelpers.findAndHookMethod(classSecurityPolicy, "a", classPolicy, 
+    	//gfe version dated 02-Jun-2014
+		classSecurityPolicy = XposedHelpers.findClass("g.agc", lpparam.classLoader);
+		if (classSecurityPolicy!=null) {
+			XposedBridge.log("class found : g.agc");
+
+			de.robv.android.xposed.XC_MethodHook.Unhook u;			
+			try
+			{
+				u = XposedHelpers.findAndHookMethod(classSecurityPolicy, "a", classPolicy, 
+						XC_MethodReplacement.returnConstant(Integer.valueOf("0"))
+        			);
+			}
+			catch (java.lang.Throwable e)
+			{
+				u = null; 
+				XposedBridge.log("hook1 g.agc fail");
+				
+			}				
+       	
+        	if (u!=null) {
+        		XposedBridge.log("hook1 successful");
+        	} else {
+        		XposedBridge.log("hook1 failed");   	
+        	}
+
+		
+		} else {
+			XposedBridge.log("class not found : g.agc");   	
+		}
+
+		//this attempts a more generic hook in case the above stuff fails
+    	final Class<?> classPolicy2 = XposedHelpers.findClass("com.good.android.compliance.d", lpparam.classLoader);  	   	
+
+    	if (classPolicy2!=null) {
+    		XposedBridge.log("class found : com.good.android.compliance.d");
+    	} else {   	
+    		XposedBridge.log("class not found : com.good.android.compliance.d");
+    	}    	
+    	
+    	de.robv.android.xposed.XC_MethodHook.Unhook u2;
+		
+		try
+		{	
+			u2 = XposedHelpers.findAndHookMethod(classPolicy2, "a", 
     			XC_MethodReplacement.returnConstant(Integer.valueOf("0"))
     			);
-    	
-    	if (u!=null) {
-    		XposedBridge.log("hook1 successfull");
+		}
+		catch (java.lang.Throwable e)
+		{
+			u2 = null;		
+		}
+		
+    	if (u2!=null) {
+    		XposedBridge.log("hook2 successfull");
     	} else {
-    		XposedBridge.log("hook1 failed");   	
+    		XposedBridge.log("hook2 failed");   	
+    	}
+    	
+    	de.robv.android.xposed.XC_MethodHook.Unhook u3;
+    	
+    	try
+    	{
+    		u3 = XposedHelpers.findAndHookMethod(classPolicy2, "a", int.class, XC_MethodReplacement.returnConstant(Boolean.valueOf(false))
+    			);
+    	}
+    	catch (java.lang.Throwable e)
+    	{
+    		u3 = null;
+    	}
+    	
+    	if (u3!=null) {
+    		XposedBridge.log("hook3 successfull");
+    	} else {
+    		XposedBridge.log("hook3 failed");   	
     	}
 
     	/* this patch was intended to try and disable the lock password - but it does not work and should not be included	
